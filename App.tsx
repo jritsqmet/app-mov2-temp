b5634b619b11ce856ce3d3bd725b868f0db764dd
andreeeli88 <andreeli10@hotmail.com>
2024-06-29 08:13:05 -0500

    mis cambios

--- App.tsx
+++ App.tsx
@@ -22,6 +22,6 @@ const styles = StyleSheet.create({
   textito:{
     fontSize: 50,
     color: 'pink',
-    
+
   }
 });
---
+++ screens/VentanaEliana.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function VentanaEliana() {
+  return (
+    <View>
+      <Text>VentanaEliana</Text>
+    </View>
+  )
+}
